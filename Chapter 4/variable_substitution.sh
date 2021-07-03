1dc7f19b84cd29f7330b5be618da220fa1783948
I-Tell-Myself-Secrets <86380705+I-Tell-Myself-Secrets@users.noreply.github.com>
2021-07-03 01:05:24 +0000

    Created Chapter 4
    Added:
    - variable_substitution.sh

---
+++ Chapter 4/variable_substitution.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Variable Substitution
+
+# The name of a variable is a placeholder for its value, the data it holds. Referencing (retrieving) its value is called variable substitution.
+
+# In order to carefully distinguish between the name of a variable and its value, the following must be noted: variable1 is the name of a variable, and $variable1 is a reference to its value, the data or item it contians.
+variable1=100
+echo variable1	# variable1
+echo $variable1 # 100
